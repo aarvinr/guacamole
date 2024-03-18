010a753a9b01920f2dd29d420ebaa337e64ec65f
aarvin roshin <144612896+aarvinr@users.noreply.github.com>
2024-03-18 00:38:44 +0000

    feat: concat strings

--- src/guac.ts
+++ src/guac.ts
@@ -1,3 +1,7 @@
-export function guac() {
-  return "guac";
+export function guac(strings: TemplateStringsArray, ...args: string[]): string {
+  const res: string = strings.reduce((acc, str, i) => {
+    return acc + args[i - 1] + str;
+  });
+
+  return res;
 }
